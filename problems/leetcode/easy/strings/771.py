098a87cea59cfb0b0d6b50acc4ffa8fc89fe90db
Jacob Ogle <123908271+JacobOgle@users.noreply.github.com>
2023-03-28 09:02:55 -0400

    Update 771.py

--- problems/leetcode/easy/strings/771.py
+++ problems/leetcode/easy/strings/771.py
@@ -3,7 +3,7 @@ class Solution:
         retval = 0
         # O(n) solution -- I think since we have a single for loop
         for i in stones:
-            if i in jewels:
+            if i in jewels: # Does this add additional layer to the time complexity by lookup in jewels? Would this truly be O(n^2)?
                 retval += 1
 
         return retval        
